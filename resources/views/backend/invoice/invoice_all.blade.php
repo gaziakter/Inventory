f109b2b475be414d0fecc1732fbaf2c23d8f697e
Gazi Akter <gaziakterpro@gmail.com>
2023-03-22 21:21:07 +0600

    Invoice show all

--- resources/views/backend/invoice/invoice_all.blade.php
+++ resources/views/backend/invoice/invoice_all.blade.php
@@ -32,7 +32,7 @@
                                 <th>Invoice No</th>
                                 <th>Date</th>
                                 <th>Description</th>
-                                <th>Action</th>
+                                <th>Amount</th>
                             </tr>
                             </thead>
  
@@ -40,13 +40,11 @@
                                 @foreach($allData as $key => $item)
                             <tr>
                                 <td> {{ $key+1}} </td>
-                                <td>  Customer name</td> 
+                                <td> {{ $item['payment']['customer']['name'] }} </td> 
                                 <td> {{ $item->invoice_no }} </td> 
                                 <td> {{ date('d-m-Y',strtotime($item->date))  }} </td> 
                                 <td> {{ $item->description }} </td> 
-                                <td>
-                                    <a href="{{route('purchase.delete', $item->id)}}" class="btn btn-danger sm" title="Delete Data" id="delete"><i class="fas fa-trash-alt"></i></a>
-                                </td>
+                                <td>  $ {{ $item['payment']['total_amount'] }} </td>
                             </tr>
                                 @endforeach
                             </tbody>
