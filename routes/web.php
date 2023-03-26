8280455f3c768de967d6bc6f74c985decc101606
Gazi Akter <gaziakterpro@gmail.com>
2023-03-26 17:56:50 +0600

    Supplier wise stock report

--- routes/web.php
+++ routes/web.php
@@ -123,7 +123,9 @@ Route::controller(AdminController::class)->group(function () {
 // Stock All Route 
 Route::controller(StockController::class)->group(function () {
     Route::get('/stock/report', 'StockReport')->name('stock.report');
-    Route::get('/stock/report/pdf', 'StockReportPdf')->name('stock.report.pdf');  
+    Route::get('/stock/report/pdf', 'StockReportPdf')->name('stock.report.pdf'); 
+    Route::get('/stock/supplier/wise', 'StockSupplierWise')->name('stock.supplier.wise');
+    Route::get('/supplier/wise/pdf', 'SupplierWisePdf')->name('supplier.wise.pdf');  
 
 });
 
